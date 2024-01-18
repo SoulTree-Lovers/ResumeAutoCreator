244caffd9a7d804789652ded998ba95e39c7cbd3
kangseungmin <rkdtmdals999@naver.com>
2024-01-18 18:26:27 +0900

    [feat] Education DTO 생성

--- src/main/java/model/Education.java
+++ src/main/java/model/Education.java
@@ -1,4 +1,57 @@
 package model;
 
 public class Education {
+    private Integer graduationYear;
+    private String schoolName;
+    private String major;
+    private String graduationStatus;
+
+    public Education(Integer graduationYear, String schoolName, String major, String graduationStatus) {
+        this.graduationYear = graduationYear;
+        this.schoolName = schoolName;
+        this.major = major;
+        this.graduationStatus = graduationStatus;
+    }
+
+    @Override
+    public String toString() {
+        return "Education{" +
+                "graduationYear=" + graduationYear +
+                ", schoolName='" + schoolName + '\'' +
+                ", major='" + major + '\'' +
+                ", graduationStatus='" + graduationStatus + '\'' +
+                '}';
+    }
+
+    public Integer getGraduationYear() {
+        return graduationYear;
+    }
+
+    public void setGraduationYear(Integer graduationYear) {
+        this.graduationYear = graduationYear;
+    }
+
+    public String getSchoolName() {
+        return schoolName;
+    }
+
+    public void setSchoolName(String schoolName) {
+        this.schoolName = schoolName;
+    }
+
+    public String getMajor() {
+        return major;
+    }
+
+    public void setMajor(String major) {
+        this.major = major;
+    }
+
+    public String getGraduationStatus() {
+        return graduationStatus;
+    }
+
+    public void setGraduationStatus(String graduationStatus) {
+        this.graduationStatus = graduationStatus;
+    }
 }
