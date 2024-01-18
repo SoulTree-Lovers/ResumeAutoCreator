97f1c7bf42de56f31a7782f3f31eed40e56b20f9
kangseungmin <rkdtmdals999@naver.com>
2024-01-18 19:04:58 +0900

    [refactor] View 메소드가 모델을 반환하도록 변경

--- src/main/java/view/ResumeView.java
+++ src/main/java/view/ResumeView.java
@@ -8,66 +8,64 @@ import model.PersonInfo;
 
 public class ResumeView {
     private Scanner scanner;
-    private Career career;
-    private Education education;
-    private Introduction introduction;
-    private PersonInfo personInfo;
 
-    public ResumeView(Career career, Education education, Introduction introduction, PersonInfo personInfo) {
+    public ResumeView() {
         this.scanner = new Scanner(System.in);
-        this.career = career;
-        this.education = education;
-        this.introduction = introduction;
-        this.personInfo = personInfo;
     }
 
-    public void inputPersonInfo() {
+    public PersonInfo inputPersonInfo() {
         System.out.println("<개인정보 입력>");
 
         System.out.print("이름을 입력하세요: ");
-        personInfo.setName(scanner.nextLine());
+        String name = scanner.nextLine();
         System.out.print("이메일 주소를 입력하세요: ");
-        personInfo.setEmail(scanner.nextLine());
+        String email = scanner.nextLine();
         System.out.print("집 주소를 입력하세요: ");
-        personInfo.setAddress(scanner.nextLine());
+        String address = scanner.nextLine();
         System.out.print("전화번호를 입력하세요: ");
-        personInfo.setPhoneNumber(scanner.nextLine());
+        String phoneNumber = scanner.nextLine();
         System.out.print("생년월일을 입력하세요: ");
-        personInfo.setBirthDate(scanner.nextLine());
+        String birthDate = scanner.nextLine();
         System.out.print("사진 링크 주소를 입력하세요: ");
-        personInfo.setPhoto(scanner.nextLine());
+        String photo = scanner.nextLine();
+        return new PersonInfo(photo, name, email, address, phoneNumber, birthDate);
     }
 
-    public void inputEducation() {
+    public Education inputEducation() {
         System.out.println("<학력 정보 입력>");
 
         System.out.print("학교명을 입력하세요: ");
-        education.setSchoolName(scanner.nextLine());
+        String schoolName = scanner.nextLine();
         System.out.print("전공을 입력하세요: ");
-        education.setMajor(scanner.nextLine());
+        String major = scanner.nextLine();
         System.out.print("졸업여부를 입력하세요: ");
-        education.setGraduationStatus(scanner.nextLine());
+        String graduationStatus = scanner.nextLine();
         System.out.print("졸업년도를 입력하세요: ");
-        education.setGraduationYear(scanner.nextLine());
+        String graduationYear = scanner.nextLine();
+
+        return new Education(graduationYear, schoolName, major, graduationStatus);
     }
 
-    public void inputCareer() {
+    public Career inputCareer() {
         System.out.println("<경력 정보 입력>");
 
         System.out.print("회사명을 입력하세요: ");
-        career.setCompanyName(scanner.nextLine());
+        String companyName = scanner.nextLine();
         System.out.print("담당업무를 입력하세요: ");
-        career.setJobTitle(scanner.nextLine());
+        String jobTitle = scanner.nextLine();
         System.out.print("근무기간을 입력하세요: ");
-        career.setEmploymentPeriod(scanner.nextInt());
+        Integer employmentPeriod = scanner.nextInt();
         scanner.nextLine();
         System.out.print("근속연수를 입력하세요: ");
-        career.setYearsAtCompany(scanner.nextInt());
+        Integer yearsAtCompany = scanner.nextInt();
         scanner.nextLine();
+
+        return new Career(companyName, jobTitle, employmentPeriod, yearsAtCompany);
     }
 
-    public void inputIntroduction() {
+    public Introduction inputIntroduction() {
         System.out.println("<자기소개서> (종료 시 엔터 키 입력)");
+        Introduction introduction = new Introduction();
 
         while (true) {
             String curr = scanner.nextLine();
@@ -80,5 +78,7 @@ public class ResumeView {
             introduction.addIntroduction(curr);
             introduction.addIntroduction("\n");
         }
+
+        return introduction;
     }
 }
